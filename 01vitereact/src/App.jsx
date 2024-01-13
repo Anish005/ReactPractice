8d9749a68d68080458da95b5c7d0f4768cf26508
Anish Panda <anishpanda31@gmail.com>
2024-01-13 20:02:06 +0530

    creating your own react and jsx

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,13 +1,14 @@
 
 import Anish from './anish'
 function App() {
-  
+  const username = 'anish panda'
 
   return (
     //we need to return only one parent element , so wrap it inside a div or rather a fragment
     <>
       <Anish />
-      <h1>how is the josh</h1>
+      {/* we dont write javascript instead we write the final outcome by curly braces {} only mention the evaluated expression */}
+      <h1>how is the josh {username}</h1> 
       <p> the josh is high sir </p>
     </>
     
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,49 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+//we can call jsx directly from below  we can do it instead of react CreateElement but a bit complicated go to open source
+//https://github.com/facebook/react/blob/main/packages/react/src/jsx/ReactJSXElement.js
+//import {jsx as _jsx} from 'react/jsx-runtime.js'
+function MyApp() {
+    return(
+        <div>
+            <h1>custom app verfication</h1>
+        </div>
+    )
+    
+}
+//end of the day MyApp is parsed as below in reactElement
+const reactElement =  {
+    type: 'a',
+    props: {
+        href:'https://google.com',
+        target:'_blank'
 
+    },
+    children:'Click me to visit google'
+}
+// after creating all the tree at last the variable injection is performed
+const anotherUser = 'ramesh sharma'
+
+const anotherElement = (
+    <a href="https://google.com" target='_blank'>Visit google</a>
+)
+//making reactElement according to the react format that is expected not as above custom made
+// react have a method called createElement that gives us an object, first args - tag, second - obj,third - evaluated expression
+//babel injects in the createElement
+const reactElement1 = React.createElement(
+    'a',
+    {href:'https://google.com' , target:'_blank'},
+    'Click me to visit google',
+    anotherUser // we cant write condtionals here as it is evaluated expression
+
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
-    <App />
-  
+
+        //<App /> 
+        // <reactElement /> // it expects a function but we are giving an object  and also here we are not writing custom react
+        //<MyApp /> // we can also write this as MyApp() but not conventional to write for proffessional purpose
+        //anotherElement // we get the result as we were expecting from the reactElement as render expects the req syntax
+        reactElement1  // works fine as it is in the norms of the react         
 )
