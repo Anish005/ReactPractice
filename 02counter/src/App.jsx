5b1535b400b1e5f2c32865e241e527b1a6760b78
Anish Panda <anishpanda31@gmail.com>
2024-01-14 19:57:48 +0530

    hooks and state intro

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,33 +1,45 @@
-import { useState } from 'react'
+import { useState } from 'react' 
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //let counter = 5 as this variable will not be reflected on the UI
+  //hook responsible for changing the state, that is the change is propagated under the UI
+  let [counter,setCounter] = useState(15)//give a default value to the useState for a variable
+  //state and UI are brought to be in sync
+  const addValue = () =>{
+    //console.log('value addded',Math.random());//for checking if the function  is working or not
+    console.log('clicked ', counter);
+    //major power of react on UI updation basically variable updation
+    //counter = counter + 1
+    //we can also pass the directly upadated value in the setCounter(counter) or like below mentioned
+    setCounter(counter + 1)
+  }
+  const removeVal = () => {
+    //part of assignment to keep a check on the counter till 0 and dont make it negative
+    if (counter === 0) {
+      console.log('this the end');
+    }
+    else{
+    console.log('clicked',counter);
+    setCounter(counter - 1)
+    }
+  }  
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>anish learning react</h1>
+      <h2>Counter Value: {counter}</h2>
+      <button
+      onClick={addValue}
+      >Add VAL{counter}</button>
+      <br />
+      <button
+      onClick={ removeVal }
+      >Sub VAL {counter}</button>
+      <p>footer: {counter}</p>
     </>
   )
 }
